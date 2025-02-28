94e7d53e168b42870d2a77a1fc3a6683a022af3a
DilanSudarshana <dilanlakshan18@gmail.com>
2025-03-01 02:48:48 +0530

    fix post ui

--- src/home-page/components/Middle/Post.vue
+++ src/home-page/components/Middle/Post.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div style="height: 60vh;" class="container rounded bg-light mt-2 mb-3">
+    <div style="height: 65vh;" class="container rounded bg-light mt-2 mb-3">
 
         <div class="row p-2">
             <div class=" d-flex justify-content-between align-items-start">
@@ -9,7 +9,7 @@
                         style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;">
                     </div>
                     <div>
-                       <p id="head-text" class="fw-bold"> Rata Anurata <br> 1h <i class="bi bi-globe"></i></p>
+                        <p id="head-text" class="fw-bold"> Rata Anurata <br> 1h <i class="bi bi-globe"></i></p>
 
                     </div>
                 </div>
@@ -24,8 +24,45 @@
             </div>
         </div>
 
-        <div id="post-image" class="row p-2 bg-dark" style="height: 40vh;">
-            
+        <div id="post-image" class="row pt-0 p-2 bg-warning" style="height: 40vh;">
+
+        </div>
+
+        <div class="row p-2">
+            <div class=" d-flex justify-content-between align-items-start mt-3">
+                <div class="d-flex justify-content-start align-items-start">
+
+                    <div class="me-1">
+                        <p style="font-size: small;"><i class="bi bi-hand-thumbs-up-fill text-primary"></i> 10</p>
+                    </div>
+
+                </div>
+                <div class="d-flex justify-content-start align-items-end ">
+                    <div class="me-2">
+                        <p style="font-size: small;">46 <i class="bi bi-chat-fill"></i></p>
+                    </div>
+                    <div>
+                        <p style="font-size: small;">128 <i class="bi bi-send-fill"></i></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class=" d-flex justify-content-between align-items-center mb-2">
+                <div>
+                    <p style="font-size: small;"><i class="bi bi-hand-thumbs-up"></i> Like</p>
+                </div>
+                <div>
+                    <p style="font-size: small;"><i class="bi bi-chat"></i> Comment</p>
+                </div>
+                <div>
+                    <p style="font-size: small;"><i class="bi bi-whatsapp"></i> Send</p>
+                </div>
+                <div>
+                    <p style="font-size: small;"><i class="bi bi-send"></i> Shair</p>
+                </div>
+            </div>
         </div>
 
     </div>
@@ -74,10 +111,12 @@ export default {
 #post-image {
     background-image: url();
 }
-#head-text{
+
+#head-text {
     font-size: 10px;
 }
-#post-image{
+
+#post-image {
     background: url('/src/assets/icons/facebook.svg');
 }
 </style>
