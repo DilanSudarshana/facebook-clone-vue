b6362b5d016dc2c1e9773f3c3efa700ea4f4f71c
DilanSudarshana <dilanlakshan18@gmail.com>
2025-02-28 23:33:16 +0530

    fix ui

--- src/home-page/components/Left/ListItems.vue
+++ src/home-page/components/Left/ListItems.vue
@@ -47,134 +47,56 @@
             </div>
         </div>
 
-        <hr>
-
-        
-
-        <h3 class="badge bg-primary">Flex item 3</h3>
-    </div>
-
-    <!-- <div class="row  pt-5 ps-4">
-
-        <div class="row d-flex rounded p-1">
-
-            <div class="col-lg-2">
-                <div class="rounded-circle me-3"
-                    style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                </div>
+        <div class="d-flex flex-row justify-content-center align-items-center">
+            <div class="rounded-circle me-3"
+                style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
             </div>
-            <div class="col-lg-8 text-start">
-                <div class="mt-2 fw-bold mt-1">
-                    <p class="text-start">Dilan Sudarshana</p>
-                </div>
+            <div class="mt-2 fw-bold mt-3">
+                <p class="text-start">Dilan Sudarshana</p>
             </div>
         </div>
 
-        <div class="row d-flex rounded p-1">
-            <div class="col-lg-2">
-                <div class="rounded-circle me-3"
-                    style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                </div>
-            </div>
-            <div class="col-lg-8 text-start">
-                <div class="mt-2 fw-bold mt-1">
-                    <p class="text-start">Friends</p>
-                </div>
-            </div>
+        <div class="d-flex flex-row justify-content-center align-items-center">
+            <hr>
         </div>
 
-        <div class="row d-flex rounded p-1">
-            <div class="col-lg-2">
-                <div class="rounded-circle me-3"
-                    style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                </div>
-            </div>
-            <div class="col-lg-8 text-start">
-                <div class="mt-2 fw-bold mt-1">
-                    <p class="text-start">Memories</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row d-flex rounded p-1">
-            <div class="col-lg-2">
-                <div class="rounded-circle me-3"
-                    style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                </div>
-            </div>
-            <div class="col-lg-8 text-start">
-                <div class="mt-2 fw-bold mt-1">
-                    <p class="text-start">Saved</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row d-flex rounded p-1">
-            <div class="col-lg-2">
-                <div class="rounded-circle me-3"
-                    style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                </div>
-            </div>
-            <div class="col-lg-8 text-start">
-                <div class="mt-2 fw-bold mt-1">
-                    <p class="text-start">Group</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row d-flex rounded p-1">
-            <div class="col-lg-2">
-                <div class="rounded-circle me-3"
-                    style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                </div>
-            </div>
-            <div class="col-lg-8 text-start">
-                <div class="mt-2 fw-bold mt-1">
-                    <p class="text-start">Videos</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row d-flex rounded p-1">
-            <div class="col-lg-2">
-                <button style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;"
-                    class="btn rounded-circle" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample"
-                    aria-expanded="false" aria-controls="collapseExample"><i
-                        class="bi bi-chevron-compact-down fw-bold"></i>
-                </button>
-            </div>
-            <div class="col-lg-8 text-start">
-                <div class="mt-2 fw-bold mt-1">
-                    See More
-                </div>
+        <div class="d-flex flex-row justify-content-center align-items-center">
+            <button style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;"
+                class="btn rounded-circle me-3" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"><i
+                    class="bi bi-chevron-compact-down fw-bold"></i>
+            </button>
+            <div class="mt-2 fw-bold mt-3">
+                <p class="text-start">See more</p>
             </div>
         </div>
 
         <div class="collapse" id="collapseExample">
 
-            <div class="row d-flex rounded p-1">
-                <div class="col-lg-2">
-                    <div class="rounded-circle"
-                        style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                    </div>
+            <div class="d-flex flex-row justify-content-center align-items-center">
+                <div class="rounded-circle me-3"
+                    style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
                 </div>
-                <div class="col-lg-8 text-start">
-                    <div class="mt-2 fw-bold mt-1">
-                        <p class="text-start">Group</p>
-                    </div>
+                <div class="mt-2 mt-3">
+                    <p class="text-start">Page 01</p>
                 </div>
             </div>
 
-            <div class="row d-flex rounded p-1">
-                <div class="col-lg-2">
-                    <div class="rounded-circle me-3"
-                        style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                    </div>
+            <div class="d-flex flex-row justify-content-center align-items-center">
+                <div class="rounded-circle me-3"
+                    style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
                 </div>
-                <div class="col-lg-8 text-start">
-                    <div class="mt-2 fw-bold mt-1">
-                        <p class="text-start">Videos</p>
-                    </div>
+                <div class="mt-2 mt-3">
+                    <p class="text-start">Page 02</p>
+                </div>
+            </div>
+
+            <div class="d-flex flex-row justify-content-center align-items-center">
+                <div class="rounded-circle me-3"
+                    style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
+                </div>
+                <div class="mt-2 mt-3">
+                    <p class="text-start">Page 03</p>
                 </div>
             </div>
 
@@ -182,77 +104,16 @@
 
     </div>
 
-    <hr>
-
-    <div class="row  pt-1 ps-4">
-
-        <div class="overflow-auto" style="height: 30vh;">
-
-            <div class="col-lg-8 text-start">
-                <div class="mt-2 mt-1">
-                    <p class="text-start fw-bold" style="color: #a2a8a4;">Your Shortcuts</p>
-                </div>
-            </div>
-
-            <div class="row d-flex rounded p-1">
-                <div class="col-lg-2">
-                    <div class="rounded me-3"
-                        style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                    </div>
-                </div>
-                <div class="col-lg-8 text-start">
-                    <div class="mt-2 mt-1">
-                        <p class="text-start">1st Page</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row d-flex rounded p-1">
-                <div class="col-lg-2">
-                    <div class="rounded me-3"
-                        style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                    </div>
-                </div>
-                <div class="col-lg-8 text-start">
-                    <div class="mt-2 mt-1">
-                        <p class="text-start">2nd Page</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row d-flex rounded p-1">
-                <div class="col-lg-2">
-                    <div class="rounded me-3"
-                        style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                    </div>
-                </div>
-                <div class="col-lg-8 text-start">
-                    <div class="mt-2 mt-1">
-                        <p class="text-start">2nd Page</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row d-flex rounded p-1">
-                <div class="col-lg-2">
-                    <div class="rounded me-3"
-                        style="width: 40px; height: 40px; overflow: hidden; background-color: #f0f0f0;">
-                    </div>
-                </div>
-                <div class="col-lg-8 text-start">
-                    <div class="mt-2 mt-1">
-                        <p class="text-start">2nd Page</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div> -->
-
-
 </template>
+
 <script>
 export default {
     name: 'ListItems'
 }
 </script>
+
+<style>
+p{
+    font-size: 12px;
+}
+</style>
