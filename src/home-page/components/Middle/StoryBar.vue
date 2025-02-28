5d11b4249b2e58798461abda42e8366d14d54032
DilanSudarshana <dilanlakshan18@gmail.com>
2025-03-01 01:21:04 +0530

    fix story bar ui

--- src/home-page/components/Middle/StoryBar.vue
+++ src/home-page/components/Middle/StoryBar.vue
@@ -1,19 +1,25 @@
 <template>
-    <div style="height: 30vh;" class="d-flex justify-content-between bg-light p-2 mt-2 rounded shadow-sm">
+    <div class="bg-light p-2 d-flex justify-content-between  align-items-center mt-2 rounded">
+        <div id="row" class="row overflow-x-scroll d-flex flex-nowrap flex-row overflow-hidden mx-1">
 
+            <div class="card m-2" style="width: 9rem;height: 12rem;">
 
-        <div class="rounded bg-dark w-50 m-1">
-            1
-        </div>
-        <div class="rounded bg-dark w-50 m-1">
-            1
-        </div>
-        <div class="rounded bg-dark w-50 m-1">
-            1
+            </div>
+
+            <div class="card m-2" style="width: 9rem;height: 12rem;">
+
+            </div>
+
+            <div class="card m-2" style="width: 9rem;height: 12rem;">
+
+            </div>
+
+            <div class="card m-2" style="width: 9rem;height: 12rem;">
+
+            </div>
         </div>
 
     </div>
-
 </template>
 
 <script>
@@ -22,3 +28,20 @@ export default {
 
 }
 </script>
+
+<style>
+.row::-webkit-scrollbar {
+    height: 8px;
+    width: 8px;
+}
+
+.row::-webkit-scrollbar-track {
+    background: rgb(0, 0, 0, 0.1);
+    border-radius: 99px;
+}
+
+.row::-webkit-scrollbar-thumb {
+    background: rgb(0, 0, 0, 0.15);
+    border-radius: 99px;
+}
+</style>
