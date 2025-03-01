433eee7d27386add8f73f105b661ab7726de7f0a
DilanSudarshana <dilanlakshan18@gmail.com>
2025-03-01 10:19:21 +0530

    add contact list

---
+++ src/home-page/components/Right/ContactList.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div class=" d-flex justify-content-between align-items-start mt-2">
+        <div class="d-flex justify-content-start align-items-start">
+            <div>
+                <h6 class="text-muted mt-1">Contacts</h6>
+
+            </div>
+        </div>
+        <div class="d-flex justify-content-start align-items-end">
+            <div class="me-3">
+                <i class="bi bi-search"></i>
+            </div>
+            <div>
+                <i class="bi bi-three-dots"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="d-flex flex-column justify-content-start align-items-start">
+
+        <div class="d-flex flex-row justify-content-center align-items-center">
+            <div class="rounded-circle me-3"
+                style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;">
+            </div>
+            <div class="mt-2 fw-bold mt-3">
+                <p class="text-start">Lochana Thatharani</p>
+            </div>
+        </div>
+
+        <div class="d-flex flex-row justify-content-center align-items-center">
+            <div class="rounded-circle me-3"
+                style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;">
+            </div>
+            <div class="mt-2 fw-bold mt-3">
+                <p class="text-start">Dilan Sudarshana</p>
+            </div>
+        </div>
+
+    </div>
+
+</template>
+
+
+<script>
+export default {
+    name: 'ContactList'
+}
+</script>
---
+++ src/home-page/components/Right/MainRight.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div class="col-md-4 bg-light mt-2  d-none d-lg-block rounded">
+        <ContactList></ContactList>
+    </div>
+</template>
+
+
+<script>
+import ContactList from './ContactList.vue';
+
+export default {
+    name: 'MainRight',
+    components: {
+        ContactList,
+    }
+}
+</script>
