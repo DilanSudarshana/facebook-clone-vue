5a98aaa82fcb04df0fc64a0a88231df28a2d0864
DilanSudarshana <dilanlakshan18@gmail.com>
2025-03-01 12:34:50 +0530

    fix middlebar icons

--- src/includes/navbar/components/RightSide.vue
+++ src/includes/navbar/components/RightSide.vue
@@ -1,18 +1,31 @@
 <template>
 
     <div class="h-100 d-flex justify-content-end align-items-center">
-        <div class="rounded-circle me-1 d-flex shadow-sm"
-            style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;">
+
+        <div class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
+            data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home"
+            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            <i class="bi bi-list fs-5"></i>
         </div>
-        <div class="rounded-circle me-1 shadow-sm d-flex"
-            style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;">
+
+        <div class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
+            data-bs-toggle="tooltip" data-bs-placement="bottom" title="Messenger"
+            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            <i class="bi bi-messenger fs-5"></i>
         </div>
-        <div class="rounded-circle me-1 shadow-sm d-flex"
-            style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;">
+
+        <div class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
+            data-bs-toggle="tooltip" data-bs-placement="bottom" title="Notification"
+            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            <i class="bi bi-bell-fill fs-5"></i>
         </div>
-        <div class="rounded-circle me-1 shadow-sm d-flex"
-            style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;">
+
+        <div class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
+            data-bs-toggle="tooltip" data-bs-placement="bottom" title="Account"
+            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            <i class="bi bi-person-circle fs-5"></i>
         </div>
+
     </div>
 
 </template>
--- src/includes/navbar/components/middle.vue
+++ src/includes/navbar/components/middle.vue
@@ -1,19 +1,23 @@
 <template>
 
-
-    <div class="h-100 d-flex justify-content-center align-items-center">
-        <div class="rounded me-2 d-flex shadow-sm align-item-center justify-content-center"
-            style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;"><i
-                class="fas fa-gamepad"></i></div>
-        <div class="rounded me-2 d-flex shadow-sm align-item-center justify-content-center"
-            style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;"></div>
-        <div class="rounded me-2 d-flex shadow-sm align-item-center justify-content-center"
-            style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;"></div>
-        <div class="rounded me-2 d-flex shadow-sm align-item-center justify-content-center"
-            style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;"></div>
+    <div class="h-100 d-flex justify-content-between align-items-center mx-5">
+        <div class="rounded d-flex shadow-sm align-items-center justify-content-center"
+            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            <i class="bi bi-house-door-fill fs-5"></i>
+        </div>
+        <div class="rounded d-flex shadow-sm align-items-center justify-content-center"
+            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            <i class="bi bi-play-btn-fill fs-5"></i>
+        </div>
+        <div class="rounded d-flex shadow-sm align-items-center justify-content-center"
+            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            <i class="bi bi-camera-reels-fill fs-5"></i>
+        </div>
+        <div class="rounded d-flex shadow-sm align-items-center justify-content-center"
+            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            <i class="bi bi-controller fs-5"></i>
+        </div>
     </div>
-
-
 
 </template>
 
