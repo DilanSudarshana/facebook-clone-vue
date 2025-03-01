a84e1e8aa622b35504fe7f515b35f2baa6156c8c
DilanSudarshana <dilanlakshan18@gmail.com>
2025-03-01 13:28:26 +0530

    add animations to right side

--- src/includes/navbar/components/RightSide.vue
+++ src/includes/navbar/components/RightSide.vue
@@ -2,27 +2,27 @@
 
     <div class="h-100 d-flex justify-content-end align-items-center">
 
-        <div class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
+        <div id="right-item" class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
             data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home"
-            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            style="width: 30px; height: 30px;">
             <i class="bi bi-list fs-5"></i>
         </div>
 
-        <div class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
+        <div id="right-item" class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
             data-bs-toggle="tooltip" data-bs-placement="bottom" title="Messenger"
-            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            style="width: 30px; height: 30px;">
             <i class="bi bi-messenger fs-5"></i>
         </div>
 
-        <div class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
+        <div id="right-item" class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
             data-bs-toggle="tooltip" data-bs-placement="bottom" title="Notification"
-            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            style="width: 30px; height: 30px;">
             <i class="bi bi-bell-fill fs-5"></i>
         </div>
 
-        <div class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
+        <div id="right-item" class="rounded-circle me-1 d-flex shadow-sm justify-content-center align-items-center"
             data-bs-toggle="tooltip" data-bs-placement="bottom" title="Account"
-            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+            style="width: 30px; height: 30px;">
             <i class="bi bi-person-circle fs-5"></i>
         </div>
 
@@ -37,3 +37,13 @@ export default {
 
 }
 </script>
+
+<style>
+#right-item {
+    transition: background-color 0.3s ease-in-out;
+}
+
+#right-item:hover {
+    background-color: #efefef;
+}
+</style>
