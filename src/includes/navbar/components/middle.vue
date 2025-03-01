76bf13cd5de29b81123fff33d97871f58fc18e0e
DilanSudarshana <dilanlakshan18@gmail.com>
2025-03-01 13:23:08 +0530

    add animation to center

--- src/includes/navbar/components/middle.vue
+++ src/includes/navbar/components/middle.vue
@@ -1,22 +1,27 @@
 <template>
 
     <div class="h-100 d-flex justify-content-between align-items-center mx-5">
-        <div class="rounded d-flex shadow-sm align-items-center justify-content-center"
-            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+
+        <div id="center-div" class="rounded d-flex align-items-center justify-content-center w-25"
+            style="width: 30px; height: 30px;">
             <i class="bi bi-house-door-fill fs-5"></i>
         </div>
-        <div class="rounded d-flex shadow-sm align-items-center justify-content-center"
-            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+
+        <div id="center-div" class="rounded d-flex align-items-center justify-content-center w-25"
+            style="width: 30px; height: 30px;">
             <i class="bi bi-play-btn-fill fs-5"></i>
         </div>
-        <div class="rounded d-flex shadow-sm align-items-center justify-content-center"
-            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+
+        <div id="center-div" class="rounded d-flex align-items-center justify-content-center w-25"
+            style="width: 30px; height: 30px;">
             <i class="bi bi-camera-reels-fill fs-5"></i>
         </div>
-        <div class="rounded d-flex shadow-sm align-items-center justify-content-center"
-            style="width: 30px; height: 30px; background-color: #f0f0f0;">
+
+        <div id="center-div" class="rounded d-flex align-items-center justify-content-center w-25"
+            style="width: 30px; height: 30px;">
             <i class="bi bi-controller fs-5"></i>
         </div>
+
     </div>
 
 </template>
@@ -28,3 +33,13 @@ export default {
 
 }
 </script>
+
+<style>
+#center-div {
+    transition: background-color 0.3s ease-in-out;
+}
+
+#center-div:hover {
+    background-color: #efefef;
+}
+</style>
