aa60c4571a1a3c6174ada63f4e1034e641270faa
DilanSudarshana <dilanlakshan18@gmail.com>
2025-03-03 13:21:25 +0530

    done profile section

--- src/profile-page/components/ProfileIntro.vue
+++ src/profile-page/components/ProfileIntro.vue
@@ -3,6 +3,11 @@
     <div class="col-lg-4 col-md-12 rounded mb-5">
         <ProfileIntroDetails></ProfileIntroDetails>
         <Photos></Photos>
+
+        <div class=" d-flex  justify-content-between align-items-start p-1 mt-1 ms-4">
+            <p class="text-muted fw-bold fs-7">Privacy  · Terms  · Advertising  · Ad choices   · Cookies  ·   · Meta © 2025</p>
+        </div>
+        
     </div>
 
     <ProfileSearch></ProfileSearch>
