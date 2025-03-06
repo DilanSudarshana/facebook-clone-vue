292cf7b41f1ea6bb37344725fd3701eafb0c1dd5
DilanSudarshana <dilanlakshan18@gmail.com>
2025-03-06 15:18:27 +0530

    fix date

--- src/signup-page/SignUpMain.vue
+++ src/signup-page/SignUpMain.vue
@@ -30,13 +30,15 @@
 
                                 <hr class="mt-0">
 
+                                <!-- Input starts sing up -->
+
                                 <div class="row pb-2">
                                     <div class="col-lg-6 col-md-6 mb-1">
-                                        <input type="email" id="form1Example1" class="form-control"
+                                        <input type="text" v-model="first_name" class="form-control"
                                             placeholder="First Name" />
                                     </div>
                                     <div class="col-lg-6 col-md-6 mb-1">
-                                        <input type="email" id="form1Example1" class="form-control"
+                                        <input type="email" v-model="last_name" class="form-control"
                                             placeholder="Last Name" />
                                     </div>
                                 </div>
@@ -45,17 +47,17 @@
                                     <p class="text-muted fs-6 ms-1">Date of birth</p>
                                     <div class="col-lg-4 col-md-6 mb-1">
                                         <div class="d-flex justify-content-center align-items-center">
-                                            <input type="number" id="dayInput" class="form-control" placeholder="DD"
-                                                min="1" max="31" />
+                                            <input type="number" v-model="date" id="dayInput" class="form-control"
+                                                placeholder="DD" min="1" max="31" />
                                         </div>
                                     </div>
                                     <div class="col-lg-4 col-md-6 mb-1">
-                                        <input type="number" id="monthInput" class="form-control" placeholder="MM"
-                                            min="1" max="12" />
+                                        <input type="number" v-model="month" id="monthInput" class="form-control"
+                                            placeholder="MM" min="1" max="12" />
                                     </div>
                                     <div class="col-lg-4 col-md-6 mb-1">
-                                        <input type="number" id="yearInput" class="form-control" placeholder="YYYY"
-                                            min="1900" max="9999" />
+                                        <input type="number" v-model="year" id="yearInput" class="form-control"
+                                            placeholder="YYYY" min="1900" max="9999" />
                                     </div>
                                 </div>
 
@@ -64,22 +66,22 @@
 
                                     <div class="col-lg-4 col-md-6 mb-3">
                                         <div class="d-flex justify-content-start align-items-center">
-                                            <input type="radio" id="female" name="gender" value="female"
-                                                class="form-check-input" />
+                                            <input type="radio" id="female" v-model="gender" name="gender"
+                                                value="female" class="form-check-input" />
                                             <label for="female" class="form-check-label ms-2">Female</label>
                                         </div>
                                     </div>
                                     <div class="col-lg-4 col-md-6 mb-1">
                                         <div class="d-flex justify-content-start align-items-center">
-                                            <input type="radio" id="male" name="gender" value="male"
+                                            <input type="radio" id="male" v-model="gender" name="gender" value="male"
                                                 class="form-check-input" />
                                             <label for="male" class="form-check-label ms-2">Male</label>
                                         </div>
                                     </div>
                                     <div class="col-lg-4 col-md-6 mb-1">
                                         <div class="d-flex justify-content-start align-items-center">
-                                            <input type="radio" id="custom" name="gender" value="custom"
-                                                class="form-check-input" />
+                                            <input type="radio" id="custom" v-model="gender" name="gender"
+                                                value="custom" class="form-check-input" />
                                             <label for="custom" class="form-check-label ms-2">Custom</label>
                                         </div>
                                     </div>
@@ -89,7 +91,7 @@
 
                                     <div class="col-lg-12 col-md-12">
                                         <div class="d-flex justify-content-center align-items-center">
-                                            <input type="email" id="form1Example1" class="form-control"
+                                            <input type="text" v-model="mobile_or_email" class="form-control"
                                                 placeholder="Mobile number or email address" />
                                         </div>
                                     </div>
@@ -99,7 +101,7 @@
 
                                     <div class="col-lg-12 col-md-12">
                                         <div class="d-flex justify-content-center align-items-center">
-                                            <input type="password" id="form1Example1" class="form-control"
+                                            <input type="password" v-model="password" class="form-control"
                                                 placeholder="New password" />
                                         </div>
                                     </div>
@@ -123,7 +125,7 @@
                                 <div class="row pb-2">
                                     <div class="col-lg-12 col-md-12">
                                         <div class="d-flex justify-content-center align-items-center">
-                                            <button id="create-button" data-mdb-ripple-init type="submit"
+                                            <button id="create-button" v-on:click="signUp"
                                                 class="btn btn-success btn-block w-50">
                                                 <router-link to="/sign-up" style="text-decoration: none;color: black;">
                                                     <h6 class="text-light fw-bold">Create new account</h6>
@@ -156,14 +158,74 @@
 
 <script>
 import Footer from '../includes/footer/Footer.vue';
-
+import axios from 'axios';
 
 export default {
     name: 'SignUpMain',
 
     components: {
         Footer,
-    }
+    },
+
+    data() {
+        return {
+            first_name: '',
+            last_name: '',
+            date: '',
+            month: '',
+            year: '',
+            gender: '',
+            mobile_or_email: '',
+            password: '',
+        };
+    },
+
+    computed: {
+        dob: {
+            get() {
+                return `${this.date} ${this.month} ${this.year}`;
+            },
+            set(newValue) {
+                const m = newValue.match(/(\S*)\s+(.*)/);
+
+                this.date = m[1];
+                this.month = m[2];
+                this.year = m[3];
+            }
+        }
+    },
+
+    methods: {
+        async signUp() {
+
+            let result = await axios.post('http://localhost/facebook/user/store', {
+                first_name: this.first_name,
+                last_name: this.last_name,
+                dob: this.dob,
+                gender: this.gender,
+                mobile_or_email: this.mobile_or_email,
+                password: this.password,
+
+            }, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+            )
+            if (result) {
+                console.log('data inserted')
+
+                this.first_name = ""
+                this.last_name = ""
+                this.year = ""
+                this.gender = ""
+                this.mobile_or_email = ""
+                this.password = ""
+
+            }
+        }
+
+    },
 }
 </script>
 <style></style>
