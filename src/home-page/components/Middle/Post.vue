249a313ecb4db9e1377d00aeb78eca5e1d243309
DilanSudarshana <dilanlakshan18@gmail.com>
2025-03-08 15:28:27 +0530

    fix status bar ui issue

--- src/home-page/components/Middle/Post.vue
+++ src/home-page/components/Middle/Post.vue
@@ -1,76 +1,77 @@
 <template>
 
     <div v-for="post in posts" :key="post.id" class="container rounded bg-light mt-2 mb-3">
+        <router-link :to="'/profile/' + post.user_id" style="text-decoration: none;color: black;">
+            <div class="row p-2">
+                <div class=" d-flex justify-content-between align-items-start">
+                    <div class="d-flex justify-content-start align-items-start">
+                        <div class="rounded-circle me-3"
+                            style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;">
+                            <img :src="post.user_image" style="width: 30px; height: 30px;border-radius: 100px;" alt="">
+                        </div>
+                        <div>
+                            <p id="head-text" class="fw-bold">{{ post.first_name }} &nbsp;{{ post.last_name }} <br> {{
+                                post.created_at }} &nbsp;<i class="bi bi-globe"></i></p>
 
-        <div class="row p-2">
-            <div class=" d-flex justify-content-between align-items-start">
-                <div class="d-flex justify-content-start align-items-start">
-                    <div class="rounded-circle me-3"
-                        style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;">
-                        <img :src="post.user_image" style="width: 30px; height: 30px;border-radius: 100px;" alt="">
+                        </div>
                     </div>
-                    <div>
-                        <p id="head-text" class="fw-bold">{{ post.first_name }} &nbsp;{{ post.last_name }} <br> {{
-                            post.created_at }} &nbsp;<i class="bi bi-globe"></i></p>
-
-                    </div>
-                </div>
-                <div class="d-flex justify-content-start align-items-end">
-                    <div class="me-3">
-                        <i class="bi bi-three-dots"></i>
-                    </div>
-                    <div>
-                        <i class="bi bi-x-lg" v-on:click="deletePost(post.id)"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div id="post-image" class="row pt-0 p-2">
-            <p class="fs-6">{{ post.description }}</p>
-        </div>
-
-        <div id="post-image" class="row pt-0 p-2" style="height: 40vh;">
-            <img style="height: 40vh;" :src="post.image" alt="">
-        </div>
-
-        <div class="row p-2">
-            <div class=" d-flex justify-content-between align-items-start mt-3">
-                <div class="d-flex justify-content-start align-items-start">
-
-                    <div class="me-1">
-                        <p style="font-size: small;"><i class="bi bi-hand-thumbs-up-fill text-primary"></i> {{
-                            post.like_count }}</p>
-                    </div>
-
-                </div>
-                <div class="d-flex justify-content-start align-items-end ">
-                    <div class="me-2">
-                        <p style="font-size: small;">{{ post.comment_count }} <i class="bi bi-chat-fill"></i></p>
-                    </div>
-                    <div>
-                        <p style="font-size: small;">{{ post.shair_count }} <i class="bi bi-send-fill"></i></p>
+                    <div class="d-flex justify-content-start align-items-end">
+                        <div class="me-3">
+                            <i class="bi bi-three-dots"></i>
+                        </div>
+                        <div>
+                            <i class="bi bi-x-lg" v-on:click="deletePost(post.id)"></i>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="row">
-            <div class=" d-flex justify-content-between align-items-center mb-2">
-                <div v-on:click="likePost(item.id)" type="button">
-                    <p style="font-size: small;"><i class="bi bi-hand-thumbs-up"></i> Like</p>
-                </div>
-                <div>
-                    <p style="font-size: small;"><i class="bi bi-chat"></i> Comment</p>
-                </div>
-                <div>
-                    <p style="font-size: small;"><i class="bi bi-whatsapp"></i> Send</p>
-                </div>
-                <div>
-                    <p style="font-size: small;"><i class="bi bi-send"></i> Shair</p>
+            <div id="post-image" class="row pt-0 p-2">
+                <p class="fs-6">{{ post.description }}</p>
+            </div>
+
+            <div id="post-image" class="row pt-0 p-2" style="height: 40vh;">
+                <img style="height: 40vh;" :src="post.image" alt="">
+            </div>
+
+            <div class="row p-2">
+                <div class=" d-flex justify-content-between align-items-start mt-3">
+                    <div class="d-flex justify-content-start align-items-start">
+
+                        <div class="me-1">
+                            <p style="font-size: small;"><i class="bi bi-hand-thumbs-up-fill text-primary"></i> {{
+                                post.like_count }}</p>
+                        </div>
+
+                    </div>
+                    <div class="d-flex justify-content-start align-items-end ">
+                        <div class="me-2">
+                            <p style="font-size: small;">{{ post.comment_count }} <i class="bi bi-chat-fill"></i></p>
+                        </div>
+                        <div>
+                            <p style="font-size: small;">{{ post.shair_count }} <i class="bi bi-send-fill"></i></p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <div class="row">
+                <div class=" d-flex justify-content-between align-items-center mb-2">
+                    <div v-on:click="likePost(item.id)" type="button">
+                        <p style="font-size: small;"><i class="bi bi-hand-thumbs-up"></i> Like</p>
+                    </div>
+                    <div>
+                        <p style="font-size: small;"><i class="bi bi-chat"></i> Comment</p>
+                    </div>
+                    <div>
+                        <p style="font-size: small;"><i class="bi bi-whatsapp"></i> Send</p>
+                    </div>
+                    <div>
+                        <p style="font-size: small;"><i class="bi bi-send"></i> Shair</p>
+                    </div>
+                </div>
+            </div>
+        </router-link>
 
     </div>
 
--- src/home-page/components/Middle/StoryBar.vue
+++ src/home-page/components/Middle/StoryBar.vue
@@ -2,11 +2,11 @@
     <div class="bg-light p-2 d-flex justify-content-between  align-items-center mt-2 rounded">
         <div id="row" class="row overflow-x-scroll d-flex flex-nowrap flex-row overflow-hidden mx-1">
 
-            <div v-for="item in status" :key="item.id" class="card m-2"  style="width: 9rem;height: 12rem;">
-                
-                <img :src="item.image"  alt="" class="card-img-top" style="height: 10rem;">
-                <div class="bg-light mt-1">
-                    <p class="card-title text-center rounded fs-7">{{item.first_name}}</p>
+            <div v-for="item in status" :key="item.id" class="card m-2 p-0" style="width: 9rem; height: 12rem;">
+                <div :style="{ backgroundImage: 'url(' + item.image + ')', backgroundSize: 'cover', backgroundPosition: 'center', height: '10rem' }"
+                    class="card-img-top  shadow-lg"></div>
+                <div class="bg-light pb-1">
+                    <p class="card-title text-center rounded fs-7 fw-bold">{{ item.first_name }}</p>
                 </div>
             </div>
 
