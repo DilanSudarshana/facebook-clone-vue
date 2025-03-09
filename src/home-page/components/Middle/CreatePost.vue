21f7d246b937a91106ce26689aae36ce3fb94a1f
DilanSudarshana <dilanlakshan18@gmail.com>
2025-03-09 14:10:23 +0530

    add create post

--- src/home-page/components/Middle/CreatePost.vue
+++ src/home-page/components/Middle/CreatePost.vue
@@ -1,27 +1,32 @@
 <template>
-    <SearchBar data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo"></SearchBar>
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Create Post</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form>
+    <div>
+
+        <SearchBar data-bs-toggle="modal" data-bs-target="#exampleModal"></SearchBar>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Create Post</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+
                         <div class="mb-3">
-                            <label for="recipient-name" class="col-form-label">What is on your mind:</label>
-                            <input type="text" class="form-control" id="recipient-name">
+                            <label for="post-text" class="col-form-label">What is on your mind:</label>
+                            <input type="text" v-model="description" class="form-control">
                         </div>
                         <div class="mb-3">
-                            <label for="message-text" class="col-form-label">Add Images</label>
-                            <input type="image" src="" class="form-control" id="recipient-name">
+                            <label for="image-upload" class="col-form-label">Add Images</label>
+                            <input type="file" class="form-control" id="image-upload" accept="image/*"
+                                @change="onFileSelected">
                         </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Upload Image</button>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button @click="createPost" type="button" class="btn btn-primary">Upload Image</button>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
@@ -29,22 +34,58 @@
 </template>
 
 <script>
-import SearchBar from './SearchBar.vue';
+import axios from "axios";
+import SearchBar from "./SearchBar.vue";
 
 export default {
     name: "CreatePost",
-
     components: {
         SearchBar,
     },
 
     data() {
         return {
-            post: "",
-            image: "",
-            posts: [],
-        }
+            description: "",
+            image: null,
+            user_id: 1,
+            type: 1,
+        };
     },
 
-}
+    methods: {
+        onFileSelected(event) {
+            this.image = event.target.files[0];
+        },
+
+        async createPost() {
+
+            const reader = new FileReader();
+
+            reader.onload = async () => {
+                const base64Image = reader.result;
+
+                const fd = new FormData();
+                fd.append('user_id', this.user_id);
+                fd.append('type', this.type);
+                fd.append('description', this.description);
+                fd.append('image', base64Image);
+
+                try {
+                    const response = await axios.post('http://localhost/facebook/posts/create', fd, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data'
+                        }
+                    });
+                    console.log(response.data);
+                } catch (error) {
+                    console.error('Error creating post:', error.response?.data || error);
+                }
+            };
+
+            reader.readAsDataURL(this.image);
+        },
+    },
+
+
+};
 </script>
--- src/home-page/components/Middle/Post.vue
+++ src/home-page/components/Middle/Post.vue
@@ -1,10 +1,11 @@
 <template>
 
     <div v-for="post in posts" :key="post.id" class="container rounded bg-light mt-2 mb-3 shadow">
-        <router-link :to="'/profile/' + post.user_id" style="text-decoration: none;color: black;">
-            <div class="row p-2">
-                <div class=" d-flex justify-content-between align-items-start">
-                    <div class="d-flex justify-content-start align-items-start">
+
+        <div class="row p-2">
+            <div class=" d-flex justify-content-between align-items-start">
+                <div class="d-flex justify-content-start align-items-start">
+                    <router-link :to="'/profile/' + post.user_id" style="text-decoration: none;color: black;" class="d-flex ">
                         <div class="rounded-circle me-3"
                             style="width: 30px; height: 30px; overflow: hidden; background-color: #f0f0f0;">
                             <img :src="post.user_image" style="width: 30px; height: 30px;border-radius: 100px;" alt="">
@@ -14,64 +15,65 @@
                                 post.created_at }} &nbsp;<i class="bi bi-globe"></i></p>
 
                         </div>
+                    </router-link>
+                </div>
+                <div class="d-flex justify-content-start align-items-end">
+                    <div class="me-3">
+                        <i class="bi bi-three-dots"></i>
                     </div>
-                    <div class="d-flex justify-content-start align-items-end">
-                        <div class="me-3">
-                            <i class="bi bi-three-dots"></i>
-                        </div>
-                        <div>
-                            <i class="bi bi-x-lg" v-on:click="deletePost(post.id)"></i>
-                        </div>
+                    <div>
+                        <i class="bi bi-x-lg" v-on:click="deletePost(post.id)"></i>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <div id="post-image" class="row pt-0 p-2">
-                <p class="fs-6">{{ post.description }}</p>
-            </div>
+        <div id="post-image" class="row pt-0 p-2">
+            <p class="fs-6">{{ post.description }}</p>
+        </div>
 
-            <div id="post-image" class="row pt-0 p-2" style="height: 40vh;">
-                <img style="height: 40vh;" :src="post.image" alt="">
-            </div>
+        <div id="post-image" class="row pt-0 p-2" style="height: 60vh;">
+            <img style="height: 60vh;" :src="post.image" alt="">
+        </div>
 
-            <div class="row p-2">
-                <div class=" d-flex justify-content-between align-items-start mt-3">
-                    <div class="d-flex justify-content-start align-items-start">
+        <div class="row p-2">
+            <div class=" d-flex justify-content-between align-items-start mt-3">
+                <div class="d-flex justify-content-start align-items-start">
 
-                        <div class="me-1">
-                            <p style="font-size: small;"><i class="bi bi-hand-thumbs-up-fill text-primary"></i> {{
-                                post.like_count }}</p>
-                        </div>
-
+                    <div class="me-1">
+                        <p style="font-size: small;"><i class="bi bi-hand-thumbs-up-fill text-primary"></i> {{
+                            post.like_count }}</p>
                     </div>
-                    <div class="d-flex justify-content-start align-items-end ">
-                        <div class="me-2">
-                            <p style="font-size: small;">{{ post.comment_count }} <i class="bi bi-chat-fill"></i></p>
-                        </div>
-                        <div>
-                            <p style="font-size: small;">{{ post.shair_count }} <i class="bi bi-send-fill"></i></p>
-                        </div>
+
+                </div>
+                <div class="d-flex justify-content-start align-items-end ">
+                    <div class="me-2">
+                        <p style="font-size: small;">{{ post.comment_count }} <i class="bi bi-chat-fill"></i></p>
+                    </div>
+                    <div>
+                        <p style="font-size: small;">{{ post.shair_count }} <i class="bi bi-send-fill"></i></p>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <div class="row">
-                <div class=" d-flex justify-content-between align-items-center mb-2">
-                    <div v-on:click="likePost(item.id)" type="button">
-                        <p style="font-size: small;"><i class="bi bi-hand-thumbs-up"></i> Like</p>
-                    </div>
-                    <div>
-                        <p style="font-size: small;"><i class="bi bi-chat"></i> Comment</p>
-                    </div>
-                    <div>
-                        <p style="font-size: small;"><i class="bi bi-whatsapp"></i> Send</p>
-                    </div>
-                    <div>
-                        <p style="font-size: small;"><i class="bi bi-send"></i> Shair</p>
-                    </div>
+        <div class="row">
+            <div class=" d-flex justify-content-between align-items-center mb-2">
+                <div v-on:click="likePost(item.id)" type="button">
+                    <p style="font-size: small;"><i class="bi bi-hand-thumbs-up"></i> Like</p>
+                </div>
+                <div>
+                    <p style="font-size: small;"><i class="bi bi-chat"></i> Comment</p>
+                </div>
+                <div>
+                    <p style="font-size: small;"><i class="bi bi-whatsapp"></i> Send</p>
+                </div>
+                <div>
+                    <p style="font-size: small;"><i class="bi bi-send"></i> Shair</p>
                 </div>
             </div>
-        </router-link>
+        </div>
+
 
     </div>
 
